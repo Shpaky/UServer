51582562b569163427f3624b631593f2ed92279f
Владимир Соленков <solenkov.v@tvzavr.ru>
2019-02-04 18:27:28 +0300

    was rewrite initialization logic for 'logger' instance to create for every class by once

--- lib/LogNot.pm
+++ lib/LogNot.pm
@@ -1,6 +1,7 @@
-#!/usr/lib/perl
+#!/usr/bin/perl
 
 	package LogNot;
+	use 5.10.0;
 	use Log::Log4perl qw(get_logger :levels);
 
 
@@ -10,7 +11,7 @@
 		my $text = shift;
 		my $data = shift;
 		
-		my $logger = get_logger('Log');
+		state $logger ||= get_logger('Log');
 		$logger->info($text);
 	} 
 	sub log_warn
@@ -19,7 +20,7 @@
 		my $text = $_[0];
 		my $data = $_[1];
 
-		my $logger = get_logger('Log');
+		state $logger ||= get_logger('Log');
 		$logger->warn($text);
 	}
 	sub log_error
@@ -28,7 +29,7 @@
 		my $text = $_[0];
 		my $data = $_[1];
 		
-		my $logger = get_logger('Log');
+		state $logger ||= get_logger('Log');
 		$logger->error($text);
 	} 
 	sub not_error 
@@ -37,7 +38,7 @@
 		my $text = $_[0];
 		my $data = $_[1];
 		
-		my $logger = get_logger('Not');
+		state $logger ||= get_logger('Not');
 		$logger->error($text);
 	}
 	sub log_notice
@@ -46,7 +47,7 @@
 		my $text = $_[0];
 		my $data = $_[1];
 
-		my $logger = get_logger('LOG');
+		state $logger ||= get_logger('LOG');
 		$logger->warn($text);
 	}
 	sub not_notice
@@ -55,7 +56,7 @@
 		my $text = $_[0];
 		my $data = $_[1];
 
-		my $logger = get_logger('NOT');
+		state $logger ||= get_logger('NOT');
 		$logger->warn($text);
 	}
 	1;
