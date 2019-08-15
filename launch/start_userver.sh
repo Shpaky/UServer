7cd368e9c3c76bf1cb798e017f8a7426af3e6c17
Владимир Соленков <solenkov.v@tvzavr.ru>
2019-08-15 16:44:05 +0300

    rewrited paths to 'launch/start_userver.sh'

--- launch/start_userver.sh
+++ launch/start_userver.sh
@@ -2,7 +2,7 @@
 
 	kill -9 `ps aux | grep "userver.pl" | grep -v grep | tr -s ' ' '+' | cut -d+ -f2` 2> /dev/null
 	kill -9 `ps aux | grep "check.pl" | grep -v grep | tr -s ' ' '+' | cut -d+ -f2`  2> /dev/null
-	rm -f /tmp/userver/lock;
-	rm -f /tmp/userver/pipe;
+	rm -f /var/run/userver/lock;
+	rm -f /var/run/userver/pipe;
 	/bin/echo `/bin/date` $* >> /home/solenkov.v/UServer/launch/restart
 	/home/solenkov.v/UServer/userver.pl &
