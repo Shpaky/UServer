80f2d0146f52a97177a8d1a3d18ab1946fd2c895
Владимир Соленков <solenkov.v@tvzavr.ru>
2020-01-30 21:14:00 +0300

    was fixed 'start_userver.sh' script - added check of exist '/var/run/userver' directory

--- launch/start_userver.sh
+++ launch/start_userver.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-	kill -9 `ps aux | grep "userver.pl" | grep -v grep | tr -s ' ' '+' | cut -d+ -f2` 2> /dev/null
-	kill -9 `ps aux | grep "check.pl" | grep -v grep | tr -s ' ' '+' | cut -d+ -f2`  2> /dev/null
-	rm -f /var/run/userver/lock;
-	rm -f /var/run/userver/pipe;
-	/bin/echo `/bin/date` $* >> /home/solenkov.v/UServer/launch/restart
-	/home/solenkov.v/UServer/userver.pl &
+    kill -9 `ps aux | grep "userver.pl" | grep -v grep | tr -s ' ' '+' | cut -d+ -f2` 2> /dev/null
+    kill -9 `ps aux | grep "check.pl" | grep -v grep | tr -s ' ' '+' | cut -d+ -f2`  2> /dev/null
+    rm -f /var/run/userver/lock;
+    rm -f /var/run/userver/pipe;
+    if ! [ -d /var/run/userver ];
+    then
+        mkdir -m 766 /var/run/userver
+    fi
+    /bin/echo `/bin/date` $* >> /home/solenkov.v/UServer/launch/restart
+    /home/solenkov.v/UServer/userver.pl &
